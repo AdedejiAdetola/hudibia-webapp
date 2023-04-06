4f4685f3cef6484d7c601186f27176e4ff4a6281
Adetola Adedeji <adetolaadedeji0@gmail.com>
2023-04-06 01:09:49 +0100

    Create landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
 import './styles.css';
+import backgroundImage from '../../images/svg.svg';
+import hudibiaLogo from '../../images/logo.png';
+
 
 const Home = () => {
     return ( 
-        <div>
-            <h1>HomePage</h1>
+        <div className='homepage'>
+            <img className='backgroundImage' src={backgroundImage} alt='welcome-page' />
+
+            <div className='right'>
+                <img className='hudibia-logo' src={hudibiaLogo} alt='welcome-page' />
+                <div class='welcome-text'>
+                    <p>Welcome</p>
+                    <p>to Hudibia</p>
+                </div>
+                <div className='buttons'>
+                    <button className='btn'>Login</button>
+                    <button className='btn'>Signup</button>
+                </div>
+            </div>
         </div>
      );
 }
