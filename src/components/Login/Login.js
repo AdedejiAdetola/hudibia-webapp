9bb7e60b10adced47858d1855bc40985797e2cb4
Adetola Adedeji <adetolaadedeji0@gmail.com>
2023-04-06 03:06:50 +0100

    Create login page

---
+++ src/components/Login/Login.js
@@ -0,0 +1,36 @@
+import hudibiaLogo from '../../images/logo.png';
+import './styles.css';
+
+const Login = () => {
+    return ( 
+        <div className='login'>
+            <img className='hudibia-logo' src={hudibiaLogo} alt='welcome-page' />
+
+            <div className='login-container'>
+                <p className='header'>Login</p>
+
+                <div className='input-div-container'>
+                    <div className='input-div'>
+                        <label for='email'>Email</label>
+                        <input id='email' type='email' placeholder='Enter your email' required/>
+                    </div>
+
+                    <div className='input-div'>
+                        <label for='password'>Password</label>
+                        <input id='password' type='password' placeholder='Enter your password' required/>
+                    </div>
+                </div>
+                
+
+                <p className='forgot-password'>Having issues with Login? <span>Forgot Password</span></p>
+
+                <button className='btn'>Login</button>
+
+                <p className='account'>Don't have an account? <span>Sign Up</span></p>
+
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
